611339a2e4b58a7c63ff947b6b7b55efa2b3cb78
Harrison Strowd <hstrowd@clairvista.com>
2015-04-16 20:22:24 -0500

    Updates sessions to use a Redis server instead of the default cookie store.

--- Gemfile
+++ Gemfile
@@ -38,3 +38,6 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+
+# Manage Sessions via Redis
+gem 'redis-rails'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_session-test_session'
+# Rails.application.config.session_store :cookie_store, key: '_session-test_session'
+Rails.application.config.redis_connection_params = {
+  :host => "localhost",
+  :port => 6379,
+  :db => 0,
+  :expires_in => 90.minutes
+}
+Rails.application.config.session_store :redis_store, Rails.application.config.redis_connection_params
