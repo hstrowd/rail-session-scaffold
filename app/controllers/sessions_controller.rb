4fa8b4c0dfb72045c99f4e5d691c2ce0cc926065
Harrison Strowd <hstrowd@clairvista.com>
2015-04-18 08:31:19 -0500

    Adds support for looking up and loading an existing Redis session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,32 @@ class SessionsController < ApplicationController
   def create
     if !params[:id]
       render status: :bad_request
+      return
     end
 
-    render status: :not_implemented, json: {
-      error: 'Direct loading of sessions is not supported for the cookie session store.'
-    }
+    # TODO: Should this connection be persisted and shared across processes?
+    redisStore = ActiveSupport::Cache::RedisStore.new(Rails.application.config.redis_connection_params)
+    requestedSession = redisStore.read(params[:id])
+
+    if !requestedSession
+      render status: :bad_request, json: {
+        error: "No session found with ID '#{params[:id]}'"
+      }
+      return
+    end
+
+    # NOTE: This is a hack that should never be done in a production app,
+    #   but allows us to reuse another active session and operate on it
+    #   via the frontend controls exposed. These operations should only be
+    #   performed programatically in a production app.
+    session.id = params[:id]
+    session.replace(requestedSession)
   end
 
   def update
     if !params[:key]
       render status: :bad_request, json: { error: 'Missing key parameter.'}
+      return
     end
 
     session[params[:key]] = params[:value]
